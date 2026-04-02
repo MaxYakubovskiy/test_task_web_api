5ad4ad794fced0bfd432e6b556a723eb33f94529
maxyakubovskiy <yakubovskijm@gmail.com>
2026-04-02 11:29:21 +0300

    fix models

--- src/currency/models.py
+++ src/currency/models.py
@@ -9,12 +9,12 @@ class Currency(models.Model):
 
 class CurrencyRate(models.Model):
     currency = models.ForeignKey(
-        Currency,
+        "Currency",
         on_delete=models.CASCADE,
         related_name="rates",
     )
     rate = models.DecimalField(max_digits=10, decimal_places=4)
-    creates_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         ordering = ["-created_at"]
