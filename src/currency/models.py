2894cbc13ea8165bcb2f39699240bcc31346d2ef
maxyakubovskiy <yakubovskijm@gmail.com>
2026-04-02 11:24:04 +0300

    create models

--- src/currency/models.py
+++ src/currency/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=10, unique=True, db_index=True)
+    is_active = models.BooleanField(default=True)
+
+
+class CurrencyRate(models.Model):
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.CASCADE,
+        related_name="rates",
+    )
+    rate = models.DecimalField(max_digits=10, decimal_places=4)
+    creates_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ["-created_at"]
